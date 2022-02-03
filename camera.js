8ae5711a6a9a93fe658d046aaf5e34786b4c3785
Esrafil <57289402+EsrafilElahi@users.noreply.github.com>
2022-02-04 02:25:04 +0330

    clear timing

--- camera.js
+++ camera.js
@@ -10,11 +10,11 @@ document
       const track = videoTracks[0];
       alert(`Getting video from: ${track.label}`);
       document.querySelector("video").srcObject = stream;
-      document.querySelector("#get-access").setAttribute("hidden", true);
+      // document.querySelector("#get-access").setAttribute("hidden", true);
       //The video stream is stopped by track.stop() after 3 second of playback.
-      setTimeout(() => {
-        track.stop();
-      }, 3 * 1000);
+      // setTimeout(() => {
+      //   track.stop();
+      // }, 3 * 1000);
     } catch (error) {
       alert(`errori hast : ${error.name}`);
       console.error(error);
