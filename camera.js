9a8fcf6be316ddf28eb1a4b5d990d5883ff029a5
Esrafil <57289402+EsrafilElahi@users.noreply.github.com>
2022-02-04 01:56:52 +0330

    camera 3

--- camera.js
+++ camera.js
@@ -13,21 +13,15 @@
 //   });
 // }
 
-document.querySelector('#get-access').addEventListener('click', async function init(e) {
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia({
-      audio: false,
-      video: true
-    })
-    const videoTracks = stream.getVideoTracks()
-    const track = videoTracks[0]
-    alert(`Getting video from: ${track.label}`)
-    document.querySelector('video').srcObject = stream
-    document.querySelector('#get-access').setAttribute('hidden', true)
-//The video stream is stopped by track.stop() after 3 second of playback.
-    setTimeout(() => { track.stop() }, 3 * 1000)
-  } catch (error) {
-    alert(`errori hast : ${error.name}`)
-    console.error(error)
-  }
-})
+document
+  .querySelector("#get-access")
+  .addEventListener("click", async function init(e) {
+    navigator.mediaDevices.getUserMedia({
+      video: {
+        minAspectRatio: 1.333,
+        minFrameRate: 30,
+        width: 1280,
+        heigth: 720,
+      },
+    });
+  });
