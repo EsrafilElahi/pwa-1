1d9eb304a21b7cd5845d3b63d53545747ee230b5
Esrafil <57289402+EsrafilElahi@users.noreply.github.com>
2022-02-18 00:10:02 +0330

    add camera

--- camera.js
+++ camera.js
@@ -1,64 +1,97 @@
-// notification
+// const getMedia = async () => {
+//   let videoElement = document.getElementById("video");
 
+//   // doesn't support
+//   if (!("mediaDevices" in navigator)) {
+//     console.log("MediaDevices doesn't support in your browser");
+//     return;
+//   }
 
+//   // check old version
+//   if (navigator.mediaDevices.getUserMedia === undefined) {
+//     navigator.mediaDevices.getUserMedia = function (constraints) {
+//       var getUserMedia =
+//         navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
+//       if (!getUserMedia) {
+//         console.log("again doesn't suuport");
+//       }
 
-// camera
-const getMedia = async () => {
-  // doesn't support
-  if (!("mediaDevices" in navigator)) {
-    console.log("MediaDevices doesn't support in your browser");
-    return;
-  }
+//       return new Promise(function (resolve, reject) {
+//         getUserMedia.call(navigator, constraints, resolve, reject);
+//       });
+//     };
+//   }
 
-  // check old version
-  if (navigator.mediaDevices.getUserMedia === undefined) {
-    navigator.mediaDevices.getUserMedia = function (constraints) {
-      var getUserMedia =
-        navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+//   // check current version device
+//   const checkVideoDevice = async () => {
+//     let devices = await navigator.mediaDevices.enumerateDevices();
+//     let audio = false;
+//     let video = false;
 
-      if (!getUserMedia) {
-        console.log("again doesn't suuport");
-      }
+//     console.log("devices :", devices);
 
-      return new Promise(function (resolve, reject) {
-        getUserMedia.call(navigator, constraints, resolve, reject);
-      });
-    };
-  }
+//     devices.map((device) => {
+//       if (device.kind === "audioinput") {
+//         console.log("audio :", device.kind);
+//         audio = true;
+//       } else if (device.kind === "videoinput") {
+//         console.log("video :", device.kind);
+//         video = true;
+//       }
+//     });
 
-  // check current version device
-  const checkVideoDevice = async () => {
-    let devices = await navigator.mediaDevices.enumerateDevices();
-    let audio = false;
-    let video = false;
+//     return audio, video;
+//   };
 
-    devices.map((device) => {
-      if (device.kind === "audioinput") {
-        audio = true;
-      } else if (device.kind === "videoinput") {
-        video = true;
-      }
-    });
+//   // play video
+//   let { video } = await checkVideoDevice();
+//   if (video) {
+//     let constraints = { audio: false, video: true };
+//     navigator.mediaDevices
+//       .getUserMedia(constraints)
+//       .then(function (stream) {
+//         videoElement.srcObject = stream;
+//       })
+//       .catch(function (err) {
+//         console.log("error : ", err);
+//       });
+//   } else {
+//     console.log("i can't find any video device");
+//   }
+// };
 
-    return audio, video;
-  };
+// // await getMedia();
+// async function startCapture(displayMediaOptions) {
+//   let captureStream = null;
 
-  // play video
-  let { video } = await checkVideoDevice();
-  if (video) {
-    let setting = { audio: false, video: true };
-    navigator.mediaDevices
-      .getUserMedia(setting)
-      .then(function (stream) {
-        videoElement.srcObject = stream;
-      })
-      .catch(function (err) {
-        console.log("error : ", err);
-      });
-  } else {
-    console.log("i can't find any video device");
-  }
-};
+//   try {
+//     captureStream = await navigator.mediaDevices.getDisplayMedia(
+//       displayMediaOptions
+//     );
+//   } catch (err) {
+//     console.error("Error: " + err);
+//   }
+//   return captureStream;
+// }
 
-await getMedia();
+let camera_button = document.querySelector("#start-camera");
+let video = document.querySelector("#video");
+let click_button = document.querySelector("#click-photo");
+let canvas = document.querySelector("#canvas");
+
+camera_button.addEventListener("click", async function () {
+  let stream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false,
+  });
+  video.srcObject = stream;
+});
+
+click_button.addEventListener("click", function () {
+  canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
+  let image_data_url = canvas.toDataURL("image/jpeg");
+
+  // data url of the image
+  console.log(image_data_url);
+});
