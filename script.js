9c4bc78e44834052aea6f0d7f68d0e20229439c4
Esrafil <57289402+EsrafilElahi@users.noreply.github.com>
2022-02-17 21:00:43 +0330

    set async await

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-window.onload = () => {
+window.onload = async () => {
   if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
+    await navigator.serviceWorker
       .register("./service-worker.js")
       .then((res) => console.log("service worker registered", res))
       .catch((err) => console.log("service worker not registered", err));
