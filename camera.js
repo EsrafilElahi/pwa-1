948043a482f5eedf515c0c32aecedfd7abe3afd3
Esrafil <57289402+EsrafilElahi@users.noreply.github.com>
2022-02-17 20:58:52 +0330

    first notif

--- camera.js
+++ camera.js
@@ -1,22 +1,64 @@
-document
-  .querySelector("#get-access")
-  .addEventListener("click", async function init(e) {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: true,
-      });
-      const videoTracks = stream.getVideoTracks();
-      const track = videoTracks[0];
-      alert(`Getting video from: ${track.label}`);
-      document.querySelector("video").srcObject = stream;
-      document.querySelector("#get-access").setAttribute("hidden", true);
-      document.getElementById("stop").addEventListener("click", function () {
-        track.stop();
-      });
-    } catch (error) {
-      alert(`errori hast : ${error.name}`);
-      console.error(error);
-    }
-  });
+// notification
 
+
+
+
+// camera
+const getMedia = async () => {
+  // doesn't support
+  if (!("mediaDevices" in navigator)) {
+    console.log("MediaDevices doesn't support in your browser");
+    return;
+  }
+
+  // check old version
+  if (navigator.mediaDevices.getUserMedia === undefined) {
+    navigator.mediaDevices.getUserMedia = function (constraints) {
+      var getUserMedia =
+        navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+      if (!getUserMedia) {
+        console.log("again doesn't suuport");
+      }
+
+      return new Promise(function (resolve, reject) {
+        getUserMedia.call(navigator, constraints, resolve, reject);
+      });
+    };
+  }
+
+  // check current version device
+  const checkVideoDevice = async () => {
+    let devices = await navigator.mediaDevices.enumerateDevices();
+    let audio = false;
+    let video = false;
+
+    devices.map((device) => {
+      if (device.kind === "audioinput") {
+        audio = true;
+      } else if (device.kind === "videoinput") {
+        video = true;
+      }
+    });
+
+    return audio, video;
+  };
+
+  // play video
+  let { video } = await checkVideoDevice();
+  if (video) {
+    let setting = { audio: false, video: true };
+    navigator.mediaDevices
+      .getUserMedia(setting)
+      .then(function (stream) {
+        videoElement.srcObject = stream;
+      })
+      .catch(function (err) {
+        console.log("error : ", err);
+      });
+  } else {
+    console.log("i can't find any video device");
+  }
+};
+
+await getMedia();
--- script.js
+++ script.js
@@ -1,45 +1,48 @@
 window.onload = () => {
-  "use strict";
-
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker
       .register("./service-worker.js")
       .then((res) => console.log("service worker registered", res))
       .catch((err) => console.log("service worker not registered", err));
+  } else if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  } else {
+    let permission = await Notification.requestPermission();
+    console.log("permission :", permission);
   }
 };
 
 // for install banner
-let defferPrompt;
+// let defferPrompt;
 
-window.addEventListener("beforeinstallprompt", (e) => {
-  e.preventDefault();
-  console.log("before install prompt");
-  defferPrompt = e;
-});
+// window.addEventListener("beforeinstallprompt", (e) => {
+//   e.preventDefault();
+//   console.log("before install prompt");
+//   defferPrompt = e;
+// });
 
-let btn = document.getElementById("add");
+// let btn = document.getElementById("add");
 
-btn.addEventListener("click", (e) => {
-  e.preventDefault();
+// btn.addEventListener("click", (e) => {
+//   e.preventDefault();
 
-  if (defferPrompt) {
-    console.log("user choice", defferPrompt.userChoice);
-    defferPrompt.userChoice.then((res) => {
-      if (res.outcome === "accepted") {
-        console.log("accepted");
-      } else {
-        console.log("not accepted");
-      }
-      defferPrompt = null;
-    });
-  }
-});
+//   if (defferPrompt) {
+//     console.log("user choice", defferPrompt.userChoice);
+//     defferPrompt.userChoice.then((res) => {
+//       if (res.outcome === "accepted") {
+//         console.log("accepted");
+//       } else {
+//         console.log("not accepted");
+//       }
+//       defferPrompt = null;
+//     });
+//   }
+// });
 
-window.addEventListener("appinstalled", (e) => {
-  console.log("app installed");
-  defferPrompt = null;
-});
+// window.addEventListener("appinstalled", (e) => {
+//   console.log("app installed");
+//   defferPrompt = null;
+// });
 
 // document.addEventListener("DOMContentLoaded", async (e) => {
 
